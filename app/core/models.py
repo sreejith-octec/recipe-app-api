67d40c9cd39f86612285d244711414ebecccc2e0
sreejith v <sreejithv@sreejiths-MacBook-Pro.local>
2021-05-22 11:40:46 +0530

    latest update

--- app/core/models.py
+++ app/core/models.py
@@ -28,6 +28,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
